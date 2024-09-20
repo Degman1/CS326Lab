c8be879b8afe8d93fee5a08811bd50f6a07c6ff8
David Gerard <dgredsox@gmail.com>
2024-09-20 12:47:43 -0400

    changed port from 3000 to 4000

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 app.use(cors({
-  origin: 'http://localhost:3000'
+  origin: 'http://localhost:4000'
 }));
 app.use(express.json());
 
